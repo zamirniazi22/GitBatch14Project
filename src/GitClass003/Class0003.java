94d27f9827032fd3f1484b85f3b837510ea4ecb7
zniazi <zniazi265@gmail.com>
2022-09-28 11:02:00 -0400

    commit third

--- src/GitClass003/Class0003.java
+++ src/GitClass003/Class0003.java
@@ -6,7 +6,8 @@ public class Class0003 {
 		// TODO Auto-generated method stub
 System.out.println(" hey this is git class");
 System.out.println(" hey this is a change");
-System.out.println("this is new version");
+System.out.println("gitthis is new version");
+System.out.println("another new version changed");
 	}
 
 }
